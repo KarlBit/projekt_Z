45e0e2db6d9ed2c1fece72e90a292e58bc31dc8e
Angonoka <33026971+KarlBit@users.noreply.github.com>
2024-03-01 12:50:09 +0100

    :pencil: Python Dokumentation

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print ("Hello World!")
+# Dokumentation Repository 
+# Python Version 3.10
+ 
+# Import der Bibliotheken
+
+# Klassenstruktur
+class HelloWorld:
+    def __init__(self, name):   # Konstruktor Methode welche automatisch "init"ialisiert wird.
+        self.name = name
+
+    def hello(self):
+        print ("Hello World from " + self.name)  
+
+def main():
+    Josef = HelloWorld("Karl")   # Objekt Erstellung "Name"
+    Josef.hello()                # Methode 
+
+if __name__ == "__main__":      # Can get called as a script but dif. from a module 
+    main()      
